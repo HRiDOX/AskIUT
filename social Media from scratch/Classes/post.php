98572f71011804030c70ef5dd459d62c7ffe5fdc
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2021-07-03 17:53:17 +0600

    more codes

--- social Media from scratch/Classes/post.php
+++ social Media from scratch/Classes/post.php
@@ -21,18 +21,36 @@
          return $this->error;
      }
 
+     private function get_posts($id)
+     {
+        $query = "select * from posts where userid = '$id' order by id desc limit 10";
+
+		$DB = new Database();
+		$result = $DB->read($query);
+
+		if($result)
+		{
+			return $result;
+		}else
+		{
+			return false;
+		}
+
+     }
+
      private function create_postid()
-{
+     {
 
-    $length = rand(4, 19);
-    $number =" ";
-    for ($i = 0; $i < $length; $i++) {
-        $new_rand = rand(0, 9);
-        $number = $number . $new_rand;
-    }
+            $length = rand(4, 19);
+            $number =" ";
+            for ($i = 0; $i < $length; $i++) 
+            {
+                 $new_rand = rand(0, 9);
+                 $number = $number . $new_rand;
+            }
 
-    return $number;
-}
+        return $number;
+     }
      
     
  }
