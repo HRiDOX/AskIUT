41e5299ae940c3cfe53b67252f2552ca2d524450
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2021-10-06 17:51:15 +0600

    added a new folder

---
+++ 2_ASK_iut_codes - Copy/user_member.php
@@ -0,0 +1,23 @@
+<div id="friends" style="display: inline-block;">
+    <?php
+
+    $image = "image/pic_holder.jpg";
+    if (file_exists($FRIEND_ROW['profile_image'])) {
+        //$image = $image_class->get_thumb_profile($ROW_USER['profile_image']);
+        $image = $FRIEND_ROW['profile_image'];
+    }
+
+    /* if (file_exists($FRIEND_ROW['profile_image'])) {
+        $image = ($FRIEND_ROW['profile_image']);
+    }
+*/
+
+    ?>
+
+    <a href="profile.php?id=<?php echo $FRIEND_ROW['userid']; ?>">
+        <img id="friends_img" src="<?php echo $image ?>">
+        <br>
+
+        <?php echo $FRIEND_ROW['FirstName'] . " " . $FRIEND_ROW['LastName'] ?>
+    </a>
+</div>
